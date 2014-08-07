0312f3dfbd1c2ce46e5b4117412ac04c9ed55594
PA Nilsson <pa@openproducts.se>
2014-08-07 22:20:36 +0200

    Added mysql dump again to backup set Fixed exclude paths

--- s3ql_backup.sh
+++ s3ql_backup.sh
@@ -106,12 +106,12 @@ rsync -aHAXx --delete-during --delete-excluded --partial -v \
 
 echo "Copy system files"
 rsync -aHAXx --delete-during --delete-excluded --partial -v \
-    --exclude "${owncloud_dir}" \
-    --exclude "/var/opi/mysql" \
+    --exclude "owncloud/data/" \
+    --exclude "mysql" \
     "/var/opi" "./${new_backup}/${systemdir}"
 
-#echo "Dump SQL database"
-#/usr/bin/mysqldump -uroot -p${mysql_pwd} --all-databases > "./${new_backup}/${systemdir}/opi.sql"
+echo "Dump SQL database"
+/usr/bin/mysqldump -uroot -p${mysql_pwd} --all-databases > "./${new_backup}/${systemdir}/opi.sql"
 
 # Make the new backup immutable
 # ${s3ql_path}s3qllock "$new_backup"
