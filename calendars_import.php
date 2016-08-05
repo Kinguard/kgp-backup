afa1f53aaa1f651837fa7a2a0c8a53ca092a7627
Tor Krill <tor@openproducts.se>
2016-08-05 14:59:52 +0200

    Add calendar import script
    Signed-off-by: Tor Krill <tor@openproducts.se>

---
+++ calendars_import.php
@@ -0,0 +1,54 @@
+<?php
+set_time_limit(0);
+
+require_once "lib/base.php";
+
+OCP\App::checkAppEnabled('calendar');
+
+echo "Calendar enabled\n";
+$inpath = ".";
+
+if( count( $argv ) >= 2 )
+{
+	$inpath = $argv[1];
+}
+
+echo "Using inpath: $inpath\n";
+
+$dirs = glob($inpath ."/*", GLOB_ONLYDIR | GLOB_NOSORT);
+
+foreach( $dirs as $dir )
+{
+	$user = pathinfo( $dir, PATHINFO_BASENAME );
+	
+	OC_User::setUserId($user);
+
+	$calendars = glob( $dir . "/calendars/*", GLOB_NOSORT );
+
+	foreach( $calendars as $calendar )
+	{
+		if( ! is_file( $calendar ) )
+		{
+			continue;
+		}
+
+		$name = pathinfo( $calendar, PATHINFO_BASENAME);
+		$calname = "Imported_" . pathinfo($name,PATHINFO_FILENAME);
+
+		$id = OC_Calendar_Calendar::addCalendar($user, $name);
+
+		$ical = file_get_contents( $calendar );
+
+		$importer = new OC_Calendar_Import( $ical );
+
+		$importer->setCalendarID($id);
+		$importer->setUserID($user);
+		if( ! $importer->import() )
+		{
+			OC_Calendar_Calendar::deleteCalendar($id);
+		}
+
+	}
+}
+
+
