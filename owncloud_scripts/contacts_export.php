6097dbacc00b80e616fdf6a2440276e2a678c9d7
Tor Krill <tor@openproducts.se>
2017-10-06 16:13:41 +0200

    Update contacts export to somewhat work with latest NC
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- owncloud_scripts/contacts_export.php
+++ owncloud_scripts/contacts_export.php
@@ -1,7 +1,11 @@
 <?php
+use OCA\DAV\AppInfo\Application;
+use OCA\DAV\CardDAV\CardDavBackend;
+
 set_time_limit(0);
 
 require_once "lib/base.php";
+require_once "/usr/share/php/PhpSecop/Secop.php";
 
 if (!OC::$CLI) {
 	echo "This script can be run from the command line only" . PHP_EOL;
@@ -9,10 +13,6 @@ if (!OC::$CLI) {
 }
 
 OCP\App::checkAppEnabled('contacts');
-
-echo "App enabled\n";
-
-use OCA\Contacts\App;
 
 $outpath = ".";
 
@@ -23,11 +23,29 @@ if( count( $argv ) >= 2 )
 
 echo "Using outpath: $outpath\n";
 
+// Get any possible none OP users
 $users = OCP\User::getUsers();
+
+// Get users directly out of secop
+$s = new Secop();
+$s->sockauth();
+
+list($status, $rep) = $s->getusers();
+if ( $status )
+{
+	foreach( $rep["users"] as $user )
+	{
+		$users[]=$user;
+	}
+}
+
+$app = new Application();
+$cDB = $app->getContainer()->query(CardDavBackend::class);
 
 foreach( $users as $user )
 {
-	echo "User: $user\n";
+	echo "Exporting contacts for user $user\n";
+	
 	// Ignore errors for now
 	$dir = $outpath . "/" . $user . "/files/sysbackup/contacts";
 	mkdir( $dir , 0700, true);
@@ -37,18 +55,18 @@ foreach( $users as $user )
 		continue;
 	}
 
-	$app = new App($user);
-
-	$books = $app->getAddressBooksForUser();
+	$books = $cDB->getAddressBooksForUser("principals/users/$user");
 
 	foreach( $books as $book )
 	{
-		$filename = $dir . '/' . str_replace(' ', '_', $book->getDisplayName()) . '.vcf';
+		$filename = $dir . '/' . str_replace(' ', '_', $user."_".$book["uri"]) . '.vcf';
 		echo "File: $filename\n";
 
+		$cards = $cDB->getCards( $book["id"] );
+
 		$contacts = '';
-		foreach($book->getChildren() as $i => $contact) {
-			$contacts .= $contact->serialize() . "\r\n";
+		foreach($cards as $contact) {
+			$contacts .= $contact["carddata"];
 		}
 
 		file_put_contents( $filename, $contacts);
