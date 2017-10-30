87412eaa35d99c7ac5228b00193d7c111b6ef971
Tor Krill <tor@openproducts.se>
2017-10-30 10:43:17 +0100

    Implement restore contacts and calendars
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- backup_scripts/restore_backup.sh
+++ backup_scripts/restore_backup.sh
@@ -20,32 +20,42 @@ function mp()
   mysqladmin --defaults-file=$MYSQLCONF -s ping
 }
 
-echo "Start temporary mysql"
+function startsql()
+{
 
-# Start mysql temporarily for restore
-/usr/bin/mysqld_safe --defaults-file=$MYSQLCONF > /dev/null 2>&1 &
+	echo "Start temporary mysql"
 
-echo "Wait for mysql to start up"
-t=15
-c=1
-while [ $c -lt $t ] && ! mp
-do
-  echo "Waiting ($c)"
-  (( c++ ))
-  sleep 1
-done 
+	# Start mysql temporarily for restore
+	/usr/bin/mysqld_safe --defaults-file=$MYSQLCONF > /dev/null 2>&1 &
 
-if [ $c -eq $t ]
-then
-  echo "Failed to start mysql"
-  exit 1
-fi
+	echo "Wait for mysql to start up"
+	t=15
+	c=1
+	while [ $c -lt $t ] && ! mp
+	do
+		echo "Waiting ($c)"
+		(( c++ ))
+		sleep 1
+	done
 
-echo "Restore database from backup"
-mysql --defaults-file=$MYSQLCONF < $RESTOREPATH/system/opi.sql
+	if [ $c -eq $t ]
+	then
+		echo "Failed to start mysql"
+		exit 1
+	fi
+}
 
-echo "Shut down temporary mysql instance"
-mysqladmin --defaults-file=$MYSQLCONF shutdown
+function restoredb()
+{
+	echo "Restore database from backup"
+	mysql --defaults-file=$MYSQLCONF < $RESTOREPATH/system/opi.sql
+}
+
+function stopsql()
+{
+	echo "Shut down temporary mysql instance"
+	mysqladmin --defaults-file=$MYSQLCONF shutdown
+}
 
 echo "Clean install target"
 # Make sure we have no dangling certs
@@ -57,11 +67,11 @@ echo "Restore system data"
 rsync -a --info=progress2 $RESTOREPATH/system/opi /mnt/
 
 echo "Restore user data"
-rsync -a --info=progress2 $RESTOREPATH/userdata/* /mnt/opi/owncloud/data/
+rsync -a --info=progress2 $RESTOREPATH/userdata/* /mnt/opi/nextcloud/data/
 
 echo "Fixup environment"
 # Make sure OC gets its precious stamp file
-touch /mnt/opi/owncloud/data/.ocdata
+touch /mnt/opi/nextcloud/data/.ocdata
 
 # setup correct permissions
 chmod 0755 /mnt/opi
@@ -73,13 +83,23 @@ chown -R postfix:postfix /mnt/opi/mail/*
 chown -R 5000:5000 /mnt/opi/mail/data
 chmod 0770 /mnt/opi/mail/data/
 
-chmod 0755 /mnt/opi/owncloud/
-chown -R  www-data:www-data /mnt/opi/owncloud/data/*
-chmod -R g+w /mnt/opi/owncloud/data/
+chmod 0755 /mnt/opi/nextcloud/
+chown -R  www-data:www-data /mnt/opi/nextcloud/data
+chmod -R og+w /mnt/opi/nextcloud/data
 
 chmod 0755 /mnt/opi/roundcube/
 chown -R www-data:www-data /mnt/opi/roundcube/*
 
 chown -R secop:secop /mnt/opi/secop
+
+startsql
+
+# Import contacts and calendars
+echo "Import contacts and calendars"
+
+php /usr/share/nextcloud/calendars_import.php "$RESTOREPATH/userdata"
+php /usr/share/nextcloud/contacts_import.php "$RESTOREPATH/userdata"
+
+stopsql
 
 echo "Done with restore for $RESTOREPATH"
