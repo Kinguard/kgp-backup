4647cde98571028f247cac300de23a562dc21c1c
PA Nilsson <pa@openproducts.se>
2018-09-12 14:50:14 +0200

    tranfered -> transferred

--- backup_scripts/progress.sh
+++ backup_scripts/progress.sh
@@ -20,7 +20,7 @@ if [[ -e $progressfile ]];then
 	# $3 => rate
 	# $4 => eta
 	set $line
-	transfered=$1
+	transferred=$1
 	progress=$2
 	rate=$3
 	eta=$4
@@ -29,7 +29,7 @@ fi
 # Simple check that we at lease have 4 elements....
 # also check that the filename is valid.
 if [[ -z "$eta" ]] && [[ -e $filename ]]; then
-	echo '{"status":"0", "filename":"", "progress":"0", "eta":"", "rate":"", "transfered":""}'
+	echo '{"status":"0", "filename":"", "progress":"0", "eta":"", "rate":"", "transferred":""}'
 else
-	echo '{"status":"1", "filename":"'$filename'", "progress":"'$progress'", "eta":"'$eta'", "rate":"'$rate'", "transfered":"'$transfered'"}'
+	echo '{"status":"1", "filename":"'$filename'", "progress":"'$progress'", "eta":"'$eta'", "rate":"'$rate'", "transferred":"'$transferred'"}'
 fi
